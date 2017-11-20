c7297476e5d60aeb3e9506889558fc95ed7dc6f2
Alex Johnson <notalexjohnson@gmail.com>
2017-11-20 19:21:32 +0530

    Modify migration to make it reversible on staging

--- db/migrate/20171107131744_rename_password_in_accounts.rb
+++ db/migrate/20171107131744_rename_password_in_accounts.rb
@@ -1,11 +1,7 @@
 class RenamePasswordInAccounts < ActiveRecord::Migration
   def change
     change_table :accounts, bulk: true do |t|
-      t.rename :crypted_password, :encrypted_password
-      t.change :encrypted_password, :string
-
-      t.rename :reset_password_tokens, :confirmation_token
-      t.change :confirmation_token, :string
+      t.string :encrypted_password, :confirmation_token
     end
   end
 end
