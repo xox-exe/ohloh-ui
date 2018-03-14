a5c350806804da12351e216ff75728ed4a09a1f3
Paddy <rapbhan@gmail.com>
2018-03-14 12:28:45 +0530

    Updated Docker & Docker-Compose

---
+++ docker/db.sql
@@ -0,0 +1,5 @@
+update pg_database set encoding = pg_char_to_encoding('SQL_ASCII'), datcollate = 'en_US.UTF8', datctype = 'en_US.UTF8' where datname = 'template0';
+CREATE USER openhub_user SUPERUSER;
+ALTER USER openhub_user WITH PASSWORD 'openhub_password';
+CREATE USER fis_user SUPERUSER;
+ALTER USER fis_user WITH PASSWORD 'fis_password';
